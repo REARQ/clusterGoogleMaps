e782625edcf4b4bcafe1be5920cab6bdd096b467
David Billing <david.j.billing@gmail.com>
2023-02-01 15:45:16 +0100

    Changes....

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,47 +1,80 @@
 import React from "react";
-import {
-  MarkerClusterer,
-  OverlayView,
-  useGoogleMap,
-} from "@react-google-maps/api";
+import { OverlayView, useGoogleMap } from "@react-google-maps/api";
+import useSupercluster from "use-supercluster";
 
-const Overlay = () => {
+// https://www.leighhalliday.com/google-maps-clustering
+// https://github.com/leighhalliday/use-supercluster
+
+const Overlay = (props) => {
   const map = useGoogleMap();
 
-  const locations = [
-    { lat: 57.835416128751666, lng: 12.994002083323961 },
-    { lat: 57.93542, lng: 12.8945 },
+  // Simulate the vehicles/telemetry array
+  const vehicles = [
+    {
+      id: "djb",
+      heading: 52,
+      lat: 57.735416128751666,
+      lng: 11.994002083323961,
+    },
+    { id: "other", heading: 93, lat: 57.73542, lng: 11.8945 },
   ];
 
-  const options = {
-    imagePath: "./markerclusterer/m", // so you must have m1.png, m2.png, m3.png, m4.png, m5.png and m6.png in that folder
-  };
+  const points = vehicles.map((vehicle) => ({
+    type: "Feature",
+    properties: { cluster: false, vehicleId: vehicle.id, heading: vehicle.heading },
+    geometry: {
+      type: "Point",
+      coordinates: [parseFloat(vehicle.lng), parseFloat(vehicle.lat)],
+    },
+  }));
+
+  const { clusters, supercluster } = useSupercluster({
+    points: points,
+    bounds: props.bounds,
+    zoom: props.zoom,
+    options: { radius: 75, maxZoom: 30 },
+  });
+
   return map ? (
     <>
-      <MarkerClusterer options={options}>
-        {(clusterer) =>
-          locations.map((location) => (
-            <OverlayView
-              mapPaneName={OverlayView.MAP_PANE}
-              key={location.lat}
-              setMap={map}
-              getPosition={location}
-              position={location}
-              clusterer={clusterer}
-            >
-              <span style={{ fontSize: "40px" }}>😎</span>
-            </OverlayView>
-          ))
-        }
-      </MarkerClusterer>
-      <OverlayView
+      {clusters?.map((cluster) => {
+        console.log("the cluster: ", cluster);
+        const [longitude, latitude] = cluster.geometry.coordinates;
+        const { cluster: isCluster, point_count: pointCount } =
+          cluster.properties;
+
+        return isCluster ? (
+          <OverlayView
+            key={`cluster-${cluster.id}`}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+            position={{
+              lat: latitude,
+              lng: longitude,
+            }}
+          >
+            <span style={{ fontSize: "40px" }}>C-{pointCount}</span>
+          </OverlayView>
+        ) : (
+          <OverlayView
+            key={`crime-${cluster.properties.vehicleId}`}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+            position={{
+              lat: latitude,
+              lng: longitude,
+            }}
+          >
+            <span style={{ fontSize: "40px" }}>😎{cluster.properties.vehicleId} - {cluster.properties.heading}</span>
+          </OverlayView>
+        );
+      })}
+      {/* <OverlayView
         mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
         position={{
           lat: 57.735416128751666,
           lng: 11.994002083323961,
         }}
       >
-        <span style={{ fontSize: "40px" }}>😎</span>
+        <span style={{ fontSize: "40px" }}>2😎</span>
       </OverlayView>
       <OverlayView
         mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
@@ -50,8 +83,8 @@ const Overlay = () => {
           lng: 11.8945,
         }}
       >
-        <span style={{ fontSize: "40px" }}>😎</span>
-      </OverlayView>
+        <span style={{ fontSize: "40px" }}>1😎</span>
+      </OverlayView> */}
     </>
   ) : (
     <></>
--- src/components/SimpleMap.js
+++ src/components/SimpleMap.js
@@ -1,47 +1,73 @@
-import React from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-import Overlay from './Overlay';
-import Markers from './Markers';
-import Cluster from './Cluster';
+import React, { useCallback, useState } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+import Overlay from "./Overlay";
 
 const containerStyle = {
-  width: '900px',
-  height: '600px',
-  margin: "auto"
+  width: "900px",
+  height: "600px",
+  margin: "auto",
 };
 
 const center = {
-  lat: 57.735416128751666,
-  lng: 11.994002083323961
+  lat: 57.835416128751666,
+  lng: 12.994002083323961,
 };
 const SimpleMap = () => {
-
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: process.env.REACT_APP_googleMapsApiKey
+    googleMapsApiKey: process.env.REACT_APP_googleMapsApiKey,
   });
 
-    return (
-      // <LoadScript
-      //   googleMapsApiKey={ process.env.REACT_APP_googleMapsApiKey}
-      // >
-      isLoaded ?
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={10}
-        >
-          <Overlay/>
-          <Markers/>
-          
-          <Cluster/>
-          { /* Child components, such as markers, info windows, etc. */ }
-          <></>
-        </GoogleMap>
-       
-        : <></>
-      // </LoadScript>
-    )
+  const [theMap, setTheMap] = useState(null);
+  const [theBounds, setTheBounds] = useState(null);
+  const [theZoom, setTheZoom] = useState(null);
 
-}
+  const onBoundChange = useCallback(
+    function callback(zoom) {
+      if (theMap) {
+        const bounds = theMap.getBounds();
+
+        const myBounds = [
+          bounds.getSouthWest().lng(),
+          bounds.getSouthWest().lat(),
+          bounds.getNorthEast().lng(),
+          bounds.getNorthEast().lat(),
+        ];
+
+        setTheBounds(myBounds);
+        setTheZoom(theMap.zoom);
+      }
+    },
+    [theMap]
+  );
+
+  const onLoad = useCallback(function callback(map) {
+    setTheMap(map);
+  }, []);
+
+  return (
+    // <LoadScript
+    //   googleMapsApiKey={ process.env.REACT_APP_googleMapsApiKey}
+    // >
+    isLoaded ? (
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+        onBoundsChanged={onBoundChange}
+        onLoad={onLoad}
+      >
+        <Overlay bounds={theBounds} zoom={theZoom} />
+        {/* <Markers/> */}
+
+        {/* <Cluster/> */}
+        {/* Child components, such as markers, info windows, etc. */}
+        <></>
+      </GoogleMap>
+    ) : (
+      <></>
+    )
+    // </LoadScript>
+  );
+};
 export default SimpleMap;
